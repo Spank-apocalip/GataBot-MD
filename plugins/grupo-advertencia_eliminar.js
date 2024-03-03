f36d6f521cdf89c74cb283ec7dae6cb15b1a2758
Spank-apocalip <161268583+Spank-apocalip@users.noreply.github.com>
2024-03-02 21:31:50 -0300

    grupo-advertencia_eliminar.js

--- plugins/grupo-advertencia_eliminar.js
+++ plugins/grupo-advertencia_eliminar.js
@@ -4,7 +4,7 @@ let lenGB = lenguajeGB.lenguaje() == 'en' ? usedPrefix + 'on antitoxic' : usedPr
 if (!db.data.chats[m.chat].antitoxic && m.isGroup) return conn.sendButton(m.chat, wm, lenguajeGB.smsAdveu1() + lenGB, null, [[lenguajeGB.smsEncender(), lenGB]], fkontak, m) 
 
 let who
-let img = 'https://i.imgur.com/vWnsjh8.jpg'
+let img = 'https://tinyurl.com/264tfykt'
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 else who = m.chat
 let name = await conn.getName(m.sender)	
@@ -25,7 +25,7 @@ console.log(e)
 }}
 handler.help = ['addprem <@user>']
 handler.tags = ['owner']
-handler.command = /^(del|delete|eliminar|\-)advertir|quitar|warn(ing)?$/i
+handler.command = /^(d|del|delete|eliminar|\-)advertir|quitar|warn(ing)?$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
