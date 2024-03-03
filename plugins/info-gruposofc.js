47feb403e432ef55ed7bf076f071f7240a1592e5
Spank-apocalip <161268583+Spank-apocalip@users.noreply.github.com>
2024-03-03 03:32:34 -0300

    info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -68,7 +68,7 @@ await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}*/
 
-handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupgb$/i
+handler.command = /^chatsbig|grupos|bigchat|groupofc|chatdobig|chatodobig$/i
 handler.exp = 33
 
 export default handler
