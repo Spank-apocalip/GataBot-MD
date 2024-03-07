0c5ce83e35e4b33eea94adf32ab1671775c8a2c8
Spank-apocalip <161268583+Spank-apocalip@users.noreply.github.com>
2024-03-07 13:08:06 -0300

    Atualizar o grupo-info.js

--- plugins/grupo-info.js
+++ plugins/grupo-info.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, participants, groupMetadata }) => {
+/*let handler = async (m, { conn, participants, groupMetadata }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
 const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, antiLink2, modohorny, autosticker, audios, delete: del } = global.db.data.chats[m.chat]
 const groupAdmins = participants.filter(p => p.admin)
@@ -35,10 +35,10 @@ handler.help = ['infogrup']
 handler.tags = ['group']
 handler.command = /^(infogrupo|gro?upinfo|info(gro?up|gc))$/i
 handler.group = true
-export default handler
+export default handler*/
 
 
-/*let handler = async (m, { conn, participants, groupMetadata }) => {
+let handler = async (m, { conn, participants, groupMetadata }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
 const groupAdmins = participants.filter(p => p.admin) 
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
@@ -74,4 +74,4 @@ handler.help = ['infogrup']
 handler.tags = ['group']
 handler.command = /^(infogrupo|gro?upinfo|info(gro?up|gc))$/i
 handler.group = true
-export default handler*/
+export default handler
