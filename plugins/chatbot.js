fdef49f0caf8ad1cdc303f009c8158a62a15835f
Spank-apocalip <161268583+Spank-apocalip@users.noreply.github.com>
2024-03-25 03:51:44 -0300

    Update chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -30,7 +30,7 @@ conn.reply(m.chat, `*╭┅〘 ⚠️ 𝐎𝐛𝐞𝐝𝐞𝐜𝐞𝐫 𝐚𝐬 
 *╰═┅ৡৢ͜͡✦═╡ 𝐁𝐈𝐆 𝐌𝐂 𝐎𝐅𝐂 ╞═┅ৡৢ͜͡✦═╯*`, fkontak, m)}
 
 if (/^prefixo$/i.test(m.text) ) {
-conn.reply(m.chat,  `\`*aqui estar meu prefixo[.]* \``, fkontak, m)}
+conn.reply(m.chat,  `\`*aqui estar meu prefixo*➽[.] \``, fkontak, m)}
  
 if (/^quero um bot|quero bot|como crio um bot?|quero um bot?|quiero un bot|preciso de bot|solícito bot|solicito bot|preciso de um bot|necessito de um bot$/i.test(m.text) ) {
 conn.reply(m.chat,  `\`*⚡Quer um bot para seu grupo?* \`
