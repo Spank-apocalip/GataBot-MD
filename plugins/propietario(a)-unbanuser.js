342c54f6254eff5103b5de785ae6a3c88f8d4144
Spank-apocalip <161268583+Spank-apocalip@users.noreply.github.com>
2024-04-03 00:27:41 -0300

    Update propietario(a)-unbanuser.js

--- plugins/propietario(a)-unbanuser.js
+++ plugins/propietario(a)-unbanuser.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text, command, usedPrefix }) => {
-if (!text) throw `${mg}𝐌𝐀𝐑𝐐𝐔𝐄 𝐀𝐋𝐆𝐔𝐄𝐌 𝐏𝐀𝐑𝐀 𝐃𝐄𝐒𝐁𝐀𝐍𝐈𝐑\n𝐄𝐗𝐄𝐌𝐏𝐋𝐎\n*${usedPrefix + command} @tag*\n\n𝙏𝘼𝙂 𝙎𝙊𝙈𝙀𝙊𝙉𝙀 𝙏𝙊 𝙐𝙉𝘽𝘼𝙉\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} @tag*`
+if (!text) throw `${mg}𝐌𝐀𝐑𝐐𝐔𝐄 𝐀𝐋𝐆𝐔𝐄𝐌 𝐏𝐀𝐑𝐀 𝐃𝐄𝐒𝐁𝐀𝐍𝐈𝐑\n𝐄𝐗𝐄𝐌𝐏𝐋𝐎\n*${usedPrefix + command} @tag*`
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-if (!who) throw `${mg}𝐌𝐀𝐑𝐐𝐔𝐄 𝐀𝐋𝐆𝐔𝐄𝐌 𝐏𝐀𝐑𝐀 𝐃𝐄𝐒𝐁𝐀𝐍𝐈𝐑\n𝐄𝐗𝐄𝐌𝐏𝐋𝐎\n*${usedPrefix + command} @tag*\n\n𝙏𝘼𝙂 𝙎𝙊𝙈𝙀𝙊𝙉𝙀 𝙏𝙊 𝙐𝙉𝘽𝘼𝙉\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} @tag*`
+if (!who) throw `${mg}𝐌𝐀𝐑𝐐𝐔𝐄 𝐀𝐋𝐆𝐔𝐄𝐌 𝐏𝐀𝐑𝐀 𝐃𝐄𝐒𝐁𝐀𝐍𝐈𝐑\n𝐄𝐗𝐄𝐌𝐏𝐋𝐎\n*${usedPrefix + command} @tag*`
 let users = global.db.data.users
 users[who].banned = false
-conn.reply(m.chat, `${eg}𝐎 𝐔𝐒𝐔𝐀𝐑𝐈𝐎(𝐀) 𝐅𝐎𝐈 𝐃𝐄𝐒𝐁𝐀𝐍𝐈𝐃𝐎(𝐀) 🤡\n𝐀𝐆𝐎𝐑𝐀 𝐏𝐎𝐃𝐄 𝐔𝐒𝐀𝐑 ${gt}\n\n𝙏𝙃𝙀 𝙐𝙎𝙀𝙍 𝙒𝘼𝙎 𝙐𝙉𝘽𝘼𝙉 🤠\n𝙐𝙎𝙀 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿`, m)
+conn.reply(m.chat, `${eg}𝐎 𝐔𝐒𝐔𝐀𝐑𝐈𝐎(𝐀) 𝐅𝐎𝐈 𝐃𝐄𝐒𝐁𝐀𝐍𝐈𝐃𝐎(𝐀) 🤡\n𝐀𝐆𝐎𝐑𝐀 𝐏𝐎𝐃𝐄 𝐔𝐒𝐀𝐑 ${gt}`, m)
 }
 handler.help = ['unbanuser']
 handler.tags = ['owner']
